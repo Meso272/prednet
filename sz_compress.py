4d32f06e901ca6abd6661aa1adc3c0c973421faa
meso272 <liujinyang1201@126.com>
2020-09-07 22:32:35 -0700

    dwa

--- sz_compress.py
+++ sz_compress.py
@@ -1,5 +1,5 @@
 import os
 for i in range(1520,1550):
     filename="aramco-snapshot-%s.f32" % str(i)
-    command="/home/jliu447/packs/SZ/bin/sz -z -f -M REL -R 1e-3 -i aramco/%s -3 235 449 449" % filename
+    command="/home/jliu447/packs/SZ/bin/sz -z -f -M REL -R 1e-4 -i aramco/%s -3 235 449 449" % filename
     os.system(command) 
